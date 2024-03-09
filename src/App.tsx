95fa461d8eb83b3660aa501e52dab3de458f0ef5
AbdallahAyed <abdalla.ayed.2010@gmail.com>
2024-03-08 19:44:26 -0800

    Set game objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
+import { useState } from "react";
+
+type GameProps = {
+  level: number;
+  lives: number;
+  score: number;
+  speed: number;
+  active: boolean;
+  gameOver: boolean;
+};
+
 function App() {
+  const [game, setGame] = useState<GameProps>({
+    level: 1,
+    lives: 3,
+    score: 0,
+    speed: 20,
+    active: false,
+    gameOver: false,
+  });
+
+  const [ball, setBall] = useState({
+    x: 490,
+    y: 32,
+    width: 20,
+    height: 20,
+  });
+
+  const [paddle, setPaddle] = useState({
+    x: 400,
+  });
+
   return (
     <>
-      <h1>Hello my react app</h1>
+      <div className="game-board">
+        <div>
+          <div className="lives"></div>
+          <div className="">level</div>
+          <div className="">score</div>
+        </div>
+        <div className="bricks"></div>
+        <div className="paddle"></div>
+      </div>
     </>
   );
 }
